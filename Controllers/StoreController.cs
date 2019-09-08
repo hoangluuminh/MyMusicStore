9c0d5f9ac325eabd5e1d39b32ddf3462acf3b56c
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-08 10:16:14 +0700

    Initial commit

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMusicStore.Controllers {
+    public class StoreController : Controller {
+        public string Index() {
+            //return View();
+            return "<p style=\"font-weight: bold\">Hello world</p>";
+        }
+        public string Browse(string genre) {
+            //return View();
+            return HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+        }
+        public string Details(int id) {
+            //return View();
+            Console.WriteLine("id: " + id);
+            return HttpUtility.HtmlEncode("Store.Details, ID = " + id);
+        }
+    }
+}
