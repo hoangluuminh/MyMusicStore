0f4fd5258cee68563ec8f903396831c9156fcaae
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-10 11:05:56 +0700

    Added Album and Artist models

---
+++ Models/Artist.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace MyMusicStore.Models {
+    public class Artist {
+        public virtual int ArtistId { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
