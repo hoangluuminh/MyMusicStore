91c6a3865cb2ad3b46ef93ff1acebd056618d77c
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-10 19:11:31 +0700

    added Genre model

--- Models/Album.cs
+++ Models/Album.cs
@@ -1,7 +1,5 @@
-namespace MyMusicStore.Models
-{
-    public class Album
-	{
+namespace MyMusicStore.Models {
+    public class Album {
         public virtual int AlbumId { get; set; }
         public virtual int GenreId { get; set; }
         //public virtual Genre Genre { get; set; }
---
+++ Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMusicStore.Models {
+    public class Genre {
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual List<Album> Albums { get; set; }
+    }
+}
