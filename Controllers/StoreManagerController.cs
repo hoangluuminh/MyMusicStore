850b2225d84760d823642a9198420e03404f8f1f
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-13 09:58:30 +0700

    Added EntityFramework, StoreManager and basic database retrieving

---
+++ Controllers/StoreManagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMusicStore.Models;
+
+namespace MyMusicStore.Controllers {
+    public class StoreManagerController : Controller {
+        private MusicStoreDB db = new MusicStoreDB();
+        public ActionResult Index() {
+            var genres = from genre in db.Genres
+                         select genre;
+            var genresList = genres.ToList();
+            return View(genresList);
+        }
+    }
+}
---
+++ Models/MusicStoreDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MyMusicStore.Models {
+    public class MusicStoreDB : DbContext {
+        public MusicStoreDB() : base("name=MusicStore") {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
