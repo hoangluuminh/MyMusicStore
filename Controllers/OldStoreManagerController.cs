8b647878d56c0b1e2005a53af20515ce0f8ef1a6
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-14 10:48:34 +0700

    Get EF Scaffolding for StoreManagerController

---
+++ Controllers/OldStoreManagerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMusicStore.Models;
+using System.Net;
+
+namespace MyMusicStore.Controllers {
+    public class OldStoreManagerController : Controller {
+        private MusicStoreDB db = new MusicStoreDB();
+        public ActionResult Index() {
+            var albums = from album in db.Albums
+                         select album;
+            var albumsList = albums.ToList();
+            return View(albumsList);
+        }
+        public ActionResult Edit(int? id) {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Album album = db.Albums.Find(id);
+            if (album == null)
+                return HttpNotFound();
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            return View(album);
+        }
+    }
+}
