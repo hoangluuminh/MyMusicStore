226b283bd02b3e367545864fbc080ab704104c50
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-19 09:16:34 +0700

    Added model Order, migrated

--- Models/MusicStoreDB.cs
+++ Models/MusicStoreDB.cs
@@ -10,5 +10,7 @@ namespace MyMusicStore.Models {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<MyMusicStore.Models.Order> Orders { get; set; }
     }
 }
