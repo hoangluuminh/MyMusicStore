6615af3922611caa1677047da0725d7e8979a531
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-09-22 00:32:05 +0700

    Added additional DataAnnotations

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,30 +1,51 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MyMusicStore.Models {
     public class Order {
-        public int OrderId { get; set; }
         [Required]
+        [HiddenInput]
+        public int OrderId { get; set; }
+
+        [Required]
+        [ReadOnly(true)]
         public DateTime OrderDate { get; set; }
+
         [Required]
         public string Username { get; set; }
+
         [Required(ErrorMessage="Vui lòng điền {0}")]
         [DisplayName("First Name")]
         [StringLength(160)]
         public string Firstname { get; set; }
+
         [Required]
         [StringLength(160)]
         [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Address { get; set; }
+
         public string City { get; set; }
+
         public string State { get; set; }
+
         [DisplayName("Postal Code")]
+        [DataType(DataType.PostalCode)]
         public string PostalCode { get; set; }
+
         public string Country { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [ReadOnly(true)]
         public decimal Total { get; set; }
         //public List<OrderDetails> OrderDetails { get; set; }
 
